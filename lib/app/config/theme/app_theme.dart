4a02f4fd27e013052ecca7d778fb3b98240d53a4
Andres Marin <afmarin536@soy.sena.edu.co>
2023-11-15 07:41:30 -0500

    Al dia

---
+++ lib/app/config/theme/app_theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const Color _primaryColor = Color(0xFFFF5C5C);
+const Color _accentColorPirmary = Color(0xFFE65353);
+const Color _secondaryColor = Color(0xFF172929);
+const Color _accentColorSecondary = Color(0xFF386363);
+const Color _colorCta = Color(0xFFFFC107);
+const Color _accentColorCta = Color(0xFFE0A800);
+const Color _backgroundColorLight = Color(0xFFFFFFFF);
+const Color _errorColor = Color(0xFFFF0000);
+
+const List<Color> _colorTheme = [
+  _primaryColor,
+  _accentColorPirmary,
+  _secondaryColor,
+  _accentColorSecondary,
+  _colorCta,
+  _accentColorCta,
+  _backgroundColorLight,
+  _errorColor
+];
+
+class AppTheme {
+  final int selectedThemeColor;
+
+  AppTheme({
+    this.selectedThemeColor = 0
+  }) : assert(selectedThemeColor >= 0 && selectedThemeColor < _colorTheme.length);
+
+  ThemeData theme() {
+    return ThemeData(
+      useMaterial3: false,
+      colorSchemeSeed: _colorTheme[selectedThemeColor],
+    );
+  }
+}
--- lib/app/presentation/views/home_view/home_view.dart
+++ lib/app/presentation/views/home_view/home_view.dart
@@ -3,8 +3,25 @@ import 'package:flutter/material.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
+  static const String title = 'Home';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: (){},
+                child: const Text(
+                  'Botones view',
+                )
+            ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
